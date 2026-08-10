c8c384ef90d7b9efca63fade034bbef738693702
samadzadehartin-svg <samadzadehartin@gmail.com>
2026-08-10 14:12:59 +0330

    MM

--- app/tours/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-import { SiteHeader } from "@/components/site-header";
-import { TourDetails } from "@/components/tour-details";
-
-export default async function TourPage({ params }: { params: Promise<{ id: string }> }) {
-  const { id } = await params;
-  return <><SiteHeader active="home" /><TourDetails id={Number(id)} /></>;
-}
